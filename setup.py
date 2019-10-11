89ab918037b9d984fabef2f44f948098b842e2c8
Bernhard Arnold <arnobaer@users.noreply.github.com>
2019-10-11 17:23:12 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='qutie',
+    version='0.1.0',
+    py_modules=['qutie'],
+    install_requires=[
+        'PyQt5>=5.13',
+        'PyQtChart>=5.13'
+    ],
+    license="GPLv3",
+)
