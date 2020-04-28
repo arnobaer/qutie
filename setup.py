75eccb0968efa0a9a6a22e027a747f20de104ddb
Bernhard Arnold <bernhard.arnold@cern.ch>
2020-04-28 22:38:04 +0200

    updated dependencies

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='qutie',
-    version='0.1.0',
+    version='1.0.0',
     author="Bernhard Arnold",
     author_email="bernhard.arnold@oeaw.ac.at",
     description="Yet another pythonic UI library using PyQt5",
     packages=find_packages(exclude=['tests']),
     install_requires=[
-        'PyQt5>=5.8',
-        'PyQtChart>=5.8'
+        'PyQt5>=5.8'
     ],
     test_suite='tests',
     license="GPLv3",
